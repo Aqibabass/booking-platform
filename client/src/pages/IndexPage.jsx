af62eddcf8fb6c7b432fed6a46e0599a1228b787
[Aqib abass] <aqibabass7@gmail.com>
2025-01-03 20:40:25 +0530

    upto outlet

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+function IndexPage() {
+  return (
+    <div>
+        <Header/>
+      </div>
+
+  )
+}
+
+export default IndexPage
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+      login would be here
+    </div>
+  )
+}
+
+export default LoginPage
