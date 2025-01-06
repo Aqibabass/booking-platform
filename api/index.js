8a424f46201c0fef602225f766ffa8aa90f9fb62
[Aqib abass] <aqibabass7@gmail.com>
2025-01-06 15:10:40 +0530

    register preview and response under network

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const bcrypt =require('bcryptjs')
+const User = require('./models/Users.js')
+require('dotenv').config()
 const app = express();
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 
@@ -9,13 +14,29 @@ app.use(cors({
   origin: 'http://localhost:5173'
 }));
 
+
+ mongoose.connect(process.env.MONGO_URL)
+
 app.get('/test', (req, res) => {
   res.json('testggh ok');
 });
 
-app.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
-  res.json({ name, email, password });
+app.post('/register', async (req, res) => {
+  try {
+    console.log('Received request:', req.body);
+    const { name, email, password } = req.body;
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    console.log('User created:', userDoc);
+    res.json(userDoc);
+  } catch (err) {
+    console.error('Error during registration:', err);
+    res.status(500).json({ message: 'Error creating user' });
+  }
 });
+
 
 app.listen(4000);
