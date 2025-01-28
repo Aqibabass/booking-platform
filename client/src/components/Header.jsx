3d1fc6ce4e89182d0b6b011617d67998e22d8b25
[Aqib abass] <aqibabass7@gmail.com>
2025-01-28 16:15:54 +0530

    logo change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,7 @@ import { UserContext } from '@/UserContext';
 import React, { useContext, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { LuMapPinHouse } from "react-icons/lu";
 
 function Header({ handleSearch }) {
   const { user, setUser } = useContext(UserContext);
@@ -23,30 +24,13 @@ function Header({ handleSearch }) {
     <div className="bg-white -mx-6 px-8 py-4">
       <header className="flex w-auto justify-between items-center relative">
         <Link to={'/index'} className="flex items-center gap-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-7"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 
-              0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 
-              2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 
-              3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 
-              1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z"
-            />
-          </svg>
+        <LuMapPinHouse className='size-7' />
           <span className="font-bold text-xl hidden sm:block">TravelMate AI</span>
         </Link>
 
         <div className="flex items-center gap-4">
           {location.pathname === '/index' && (
-            <div className="flex bg-transparent items-center gap-2 border border-gray-300 rounded-full py-1 px-2 shadow-md shadow-gray-300">
+            <div className="flex bg-transparent items-center gap-2 border border-gray-300 rounded-full py-1 px-4 shadow-md shadow-gray-300">
               <input
                 type="search"
                 placeholder="Search destinations"
@@ -66,7 +50,7 @@ function Header({ handleSearch }) {
 
         <div className="flex items-center gap-4 hidden md:flex">
           <Link to="/stays" className="text-gray-600 hover:text-primary">Stays</Link>
-          <Link to="/experiences" className="text-gray-600 hover:text-primary">Experiences</Link>
+          <Link to="/create-trip" className="text-gray-600 hover:text-primary">Experiences</Link>
         </div>
 
         <div className="flex items-center gap-2">
